bc4803770cae415d3aa80b141bb0cafe654883a7
JaniceNova <janicenovacek2016@gmail.com>
2018-12-18 09:54:55 -0800

    survey colors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,26 +9,29 @@
 
 var People = [
     {
-      routeName: "yoda",
-      name: "Yoda",
-      role: "Jedi Master",
+      routeName: "AronKosinski",
+      name: "Aron Kosinski",
+      role: "Polish immigrant",
       age: 900,
       forcePoints: 2000
     },
     {
-      routeName: "darthmaul",
-      name: "Darth Maul",
-      role: "Sith Lord",
+      routeName: "MontagueJohnDruitt",
+      name: "Montague John Druitt",
+      role: "barrister",
       age: 200,
       forcePoints: 1200
     },
     {
-      routeName: "obiwankenobi",
-      name: "Obi Wan Kenobi",
-      role: "Jedi Master",
+      routeName: "MichaelOstrog",
+      name: "Michael Ostrog",
+      role: "Russian doctor",
       age: 55,
       forcePoints: 1350
     }
+
+
+    
   ];
 // ===============================================================================
 // ROUTING
